126f77ca75fba5dfa7ec2f08f90b3decda758e8e
eleonora <eleonora.digregorio8@gmail.com>
2021-05-11 14:33:13 +0200

    extension of parser for process description

--- MC_Parser.hs
+++ MC_Parser.hs
@@ -13,13 +13,9 @@ import qualified Text.Parsec.Token as Token
 
 ------------------- AST -------------------
 data BinOp
-  = Sum
-  | Diff
+  = Add
+  | Sub
   | Mul
-  deriving (Show)
-
-data UnaryOp
-  = Neg
   deriving (Show)
 
 data CmpOp
@@ -35,24 +31,20 @@ data BBinOp
   | Or
   deriving (Show)
 
-data BUnaryOp
-  = Not
-  deriving (Show)
-
 data Expr
   = Int Integer
   | Var String
+  | Neg Expr
   | BinOp BinOp Expr Expr
-  | UnaryOp UnaryOp Expr
   deriving (Show)
 
 data BExpr
   = T
   | F
-  | ExCond Expr
-  | CmpOp Expr Expr
+  | BVar String
+  | Not BExpr
   | BBinOp BBinOp BExpr BExpr
-  | BUnaryOp BUnaryOp BExpr
+  | CmpOp CmpOp Expr Expr
   deriving (Show)
 
 data Action
@@ -67,7 +59,7 @@ data Command
   | VarAssign String Expr
   | Concat Command Command
   | If BExpr Proc Proc
-  | While BExpr Proc Proc
+  | While BExpr Proc
   deriving (Show)
 
 data ProcName
@@ -78,9 +70,10 @@ data ProcName
 data Proc
   = Nil
   | ProcVar ProcName
-  | ActionPrefix Action Proc
-  | CommandPrefix Command Proc
-  | Restriction Proc String
+  | ActionP Action
+  | CommandP Command
+  | PrefixP Proc Proc
+  | Restriction Proc [String]
   | Relabelling Proc [(String, String)]
   | NonDetChoise Proc Proc
   | ParallelComp Proc Proc
@@ -96,7 +89,6 @@ data Stmt
   = ProcDef ProcName Proc
   | SetDef String [String]
   | VarDef String VarDef
-  | Seq [Stmt]
   deriving (Show)
 
 --------------------------------------------
@@ -112,22 +104,146 @@ statement =
 
 assignment =
   do
-    var <- procIdentifier
-    enum <- optionMaybe $ braces $ many integer
-    if isJust enum
-      then return $ ProcDef (Param var (Int 0)) Nil
-      else return $ ProcDef (Cst var) Nil
+    var <- procName
+    reservedOp "="
+    p <- process
+    return $ ProcDef var p
 
-paramAssignment =
+procName =
   do
     var <- procIdentifier
-    enum <- braces $ many integer
-    return $ ProcDef (Param var (Int 0)) Nil
+    param <- optionMaybe $braces aExpr
+    if isJust param
+      then return $ Param var (fromJust param)
+      else return $ Cst var
 
-cstAssignment =
+process =
+  summation
+
+summation =
   do
-    var <- procIdentifier
-    return $ ProcDef (Cst var) Nil
+    c <- composition
+    ms <- optionMaybe sumcompose
+    if isJust ms
+      then return $ NonDetChoise c (fromJust ms)
+      else return c
+
+sumcompose =
+  do
+    reservedOp "+"
+    summation
+
+composition =
+  do
+    p <- choice [try prefixC, try prefixA] --, reProcess]
+    mc <- optionMaybe parcompose
+    if isJust mc
+      then return $ ParallelComp p (fromJust mc)
+      else return p
+
+parcompose =
+  do
+    reservedOp "|"
+    composition
+
+prefixC =
+  do
+    c <- command
+    mp <- optionMaybe prefixcompose
+    if isJust mp
+      then return $ PrefixP (CommandP c) (fromJust mp)
+      else return (CommandP c)
+
+prefixA =
+  do
+    a <- action
+    mp <- optionMaybe prefixcompose
+    if isJust mp
+      then return $ PrefixP (ActionP a) (fromJust mp)
+      else return (ActionP a)
+
+prefixcompose =
+  do
+    reservedOp "."
+    process
+
+action =
+  try output
+    <|> input
+
+output =
+  do
+    reservedOp "'"
+    name <- identifier
+    mess <- optionMaybe $ angles identifier
+    if isJust mess
+      then return $ Output name (fromJust mess)
+      else return $ Coaction name
+
+input =
+  do
+    name <- identifier
+    mess <- optionMaybe $ angles identifier
+    if isJust mess
+      then return $ Input name (fromJust mess)
+      else return $ Action name
+
+command =
+  skip
+    <|> ass
+    <|> seqc
+    <|> ifc
+    <|> whilec
+
+skip =
+  do
+    reserved "skip"
+    return Skip
+
+ass =
+  do
+    var <- identifier
+    reservedOp ":="
+    VarAssign var <$> aExpr
+
+seqc =
+  do
+    c1 <- command
+    comma
+    Concat c1 <$> command
+
+ifc =
+  do
+    reserved "if"
+    b <- bExpr
+    t <- process
+    e <- process
+    return $ If b t e
+
+whilec =
+  do
+    reserved "while"
+    b <- bExpr
+    c <- process
+    return $ While b c
+
+--reProcess =
+--  parenProcess (reservedOp "\\") $
+--    braces many idenitifier
+--      <|> parenProcess (reservedOp "\\") procIdentifier
+--      <|> parenProcess $brackets relabellings
+--      <|> parenProcess
+
+--parenProcess =
+--  parents process
+--    <|> constantProcess
+
+--constantProcess = nilProcess <|> procName
+
+--nilProcess =
+--  do
+--    Integer(0)
+--    return Nil
 
 setDeclaration =
   do
@@ -141,7 +257,7 @@ defDeclaration =
   do
     reserved "def"
     var <- choice [identifier, procIdentifier]
-    def <- choice [rangeCmp, range, rangeBool]
+    def <- choice [try rangeCmp, try range, try rangeBool]
     return $ VarDef var def
 
 rangeCmp =
@@ -155,18 +271,25 @@ rangeCmp =
 
 range =
   do
-    li <- many integer
+    reservedOp "="
+    li <- brackets $ sepBy1 integer comma
     return $ Range li
 
 rangeBool =
   do
-    op <- relation
-    li <- many boolean
+    reservedOp "="
+    li <- brackets $ sepBy1 boolean comma
     return $ RangeBool li
 
-boolean =
-  (reservedOp "True" >> return T)
-    <|> (reservedOp "False" >> return F)
+bExpr = buildExpressionParser bOperators bTerm
+
+aExpr = buildExpressionParser aOperators aTerm
+
+rExpr = do
+  a1 <- aExpr
+  op <- relation
+  a2 <- aExpr
+  return $ CmpOp op a1 a2
 
 relation =
   (reservedOp "<" >> return Lt)
@@ -174,6 +297,32 @@ relation =
     <|> (reservedOp "<=" >> return Let)
     <|> (reservedOp ">=" >> return Get)
     <|> (reservedOp "=" >> return Eq)
+
+aOperators =
+  [ [Prefix (reservedOp "-" >> return Neg)],
+    [Infix (reservedOp "*" >> return (BinOp Mul)) AssocLeft],
+    [ Infix (reservedOp "+" >> return (BinOp Add)) AssocLeft,
+      Infix (reservedOp "-" >> return (BinOp Sub)) AssocLeft
+    ]
+  ]
+
+bOperators =
+  [ [Prefix (reservedOp "not" >> return Not)],
+    [Infix (reservedOp "and" >> return (BBinOp And)) AssocLeft],
+    [Infix (reservedOp "or" >> return (BBinOp Or)) AssocLeft]
+  ]
+
+aTerm = parens aExpr <|> liftM Var identifier <|> liftM Var procIdentifier <|> liftM Int integer
+
+bTerm =
+  parens bExpr
+    <|> boolean
+    <|> try rExpr
+    <|> liftM BVar identifier
+
+boolean =
+  (reservedOp "true" >> return T)
+    <|> (reservedOp "false" >> return F)
 
 parseSource code =
   case parse macroCaalParser "" code of
